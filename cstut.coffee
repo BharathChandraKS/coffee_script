a9267f0eefa4b8240341a6300a455a739e665fd6
Bharath Chandra <bharathchandraks@gmail.com>
2019-01-06 10:58:45 +1100

    numeric and string operations

--- cstut.coffee
+++ cstut.coffee
@@ -5,7 +5,7 @@ name = "Bharath"
 
 csOutput = document.getElementById("csoutput")
 csOutput.innerHTML = "Hello #{name}<br>"
-
+###
 aString = "I am a String"
 
 csOutput.insertAdjacentHTML('beforeend',
@@ -25,3 +25,68 @@ areyouhappy = no
 csOutput.insertAdjacentHTML('beforeend',
 'areYouhappy is a boolean<br>') if typeof areyouhappy is 'boolean'
 
+
+csOutput.insertAdjacentHTML('beforeend',
+"5+2 = #{5+2}<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"5-2 = #{5-2}<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"5*2 = #{5*2}<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"5/2 = #{5/2}<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"5%2 = #{5%2}<br>")
+
+ran = Math.floor(Math.random() * 100) + 1
+
+csOutput.insertAdjacentHTML('beforeend',
+"randNum = #{ran}<br>")
+
+
+fname = "Derek"
+lname = "Banas"
+
+# String Operations
+csOutput.insertAdjacentHTML('beforeend',
+fname + " " + lname + "<br>")
+
+
+longString = "This is a long string"
+
+csOutput.insertAdjacentHTML('beforeend',
+"String length: - " + longString.length + "<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"String Index: - " + longString.indexOf("long") + "<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"Character at: - " + longString.charAt(8) + "<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"Word at: - " + longString.slice(5,12) + "<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"Everything after: - " + longString.slice(5) + "<br>")
+
+stringReplace = longString.replace("long", "not so long")
+
+csOutput.insertAdjacentHTML('beforeend',
+"#{stringReplace}<br>")
+
+strArray = longString.split(" ")
+
+for x in strArray
+   csOutput.insertAdjacentHTML('beforeend',
+   "#{x}<br>")
+
+
+csOutput.insertAdjacentHTML('beforeend',
+"Uppercase: - " + longString.toUpperCase() + "<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"Lowercase: - " + longString.toLowerCase() + "<br>")
+###
--- cstut.js
+++ cstut.js
@@ -3,7 +3,7 @@
   /*
   My first coffee script tutorial
   */
-  var aString, areyouhappy, csOutput, largestNum, name, smallestNum;
+  var csOutput, name;
 
   name = "Bharath";
 
@@ -11,24 +11,86 @@
 
   csOutput.innerHTML = `Hello ${name}<br>`;
 
-  aString = "I am a String";
+  /*
+aString = "I am a String"
 
-  if (typeof aString === 'string') {
-    csOutput.insertAdjacentHTML('beforeend', 'astring is a String<br>');
-  }
+csOutput.insertAdjacentHTML('beforeend',
+'astring is a String<br>') if typeof aString is 'string'
 
-  largestNum = Number.MAX_VALUE;
+largestNum = Number.MAX_VALUE
+smallestNum = Number.MIN_VALUE
 
-  smallestNum = Number.MIN_VALUE;
+csOutput.insertAdjacentHTML('beforeend',
+largestNum + '<br>')
 
-  csOutput.insertAdjacentHTML('beforeend', largestNum + '<br>');
+csOutput.insertAdjacentHTML('beforeend',
+smallestNum + '<br>')
 
-  csOutput.insertAdjacentHTML('beforeend', smallestNum + '<br>');
+areyouhappy = no
 
-  areyouhappy = false;
+csOutput.insertAdjacentHTML('beforeend',
+'areYouhappy is a boolean<br>') if typeof areyouhappy is 'boolean'
 
-  if (typeof areyouhappy === 'boolean') {
-    csOutput.insertAdjacentHTML('beforeend', 'areYouhappy is a boolean<br>');
-  }
+csOutput.insertAdjacentHTML('beforeend',
+"5+2 = #{5+2}<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"5-2 = #{5-2}<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"5*2 = #{5*2}<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"5/2 = #{5/2}<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"5%2 = #{5%2}<br>")
+
+ran = Math.floor(Math.random() * 100) + 1
+
+csOutput.insertAdjacentHTML('beforeend',
+"randNum = #{ran}<br>")
+
+fname = "Derek"
+lname = "Banas"
+
+ * String Operations
+csOutput.insertAdjacentHTML('beforeend',
+fname + " " + lname + "<br>")
+
+longString = "This is a long string"
+
+csOutput.insertAdjacentHTML('beforeend',
+"String length: - " + longString.length + "<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"String Index: - " + longString.indexOf("long") + "<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"Character at: - " + longString.charAt(8) + "<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"Word at: - " + longString.slice(5,12) + "<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"Everything after: - " + longString.slice(5) + "<br>")
+
+stringReplace = longString.replace("long", "not so long")
+
+csOutput.insertAdjacentHTML('beforeend',
+"#{stringReplace}<br>")
+
+strArray = longString.split(" ")
+
+for x in strArray
+   csOutput.insertAdjacentHTML('beforeend',
+   "#{x}<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"Uppercase: - " + longString.toUpperCase() + "<br>")
+
+csOutput.insertAdjacentHTML('beforeend',
+"Lowercase: - " + longString.toLowerCase() + "<br>")
+ */
 
 }).call(this);
